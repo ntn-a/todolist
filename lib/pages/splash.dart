964fb1784e9310e58adfa5de3f9929c9b968c8e1
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-06-21 15:52:19 -0700

    Initial Commit.

---
+++ lib/pages/splash.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: const [
+            Icon(Icons.list_alt),
+            Text("TodoList"),
+          ],
+        ),
+      ),
+    );
+  }
+}
